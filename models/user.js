f17fd3ed417e0b5adce0c68033dd24ca861cdb86
Justin Brown <justin.v.brown@gmail.com>
2018-04-29 19:07:49 -0500

    Moved stylesheet

--- models/user.js
+++ models/user.js
@@ -5,7 +5,15 @@ var UserSchema = new Schema({
   first: String,
   last: String,
   email: String,
-  password: String
+  password: String,
+  answer1: Number,
+  answer2: Number,
+  answer3: Number,
+  answer4: Number,
+  answer5: Number,
+  answer6: Boolean,
+  answer7: Boolean,
+  answer8: Boolean
 });
 
 var User = mongoose.model('User', UserSchema);
